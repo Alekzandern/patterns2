1102cc2aa85ecee9095ecbc9f073038b9b4ad84f
Alekzandern <130380585+Alekzandern@users.noreply.github.com>
2023-09-10 13:37:45 +0000

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -11,6 +11,8 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
