614f2c8b0a002233f719e4b71fc18a92e42fcbfc
Alekzandern <130380585+Alekzandern@users.noreply.github.com>
2023-09-10 13:49:51 +0000

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -63,21 +63,4 @@ public class CardDeliveryTest {
                 .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(Condition.visible);
     }
-    from selenium import webdriver
-
-PROXY="176.9.119.170:8080"
-webdriver.DesiredCapabilities.CHROME['proxy'] = {
-    "httpProxy": PROXY,
-    "ftpProxy": PROXY,
-    "sslProxy": PROXY,
-    "proxyType": "MANUAL",
-
-}
-
-webdriver.DesiredCapabilities.CHROME['acceptSslCerts']=True
-
-driver =webdriver.Chrome(r".\chromedriver.exe")
-
-
-driver.get("https://www.google.com")
 }
